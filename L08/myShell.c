bc228b78c0974c8176e4aa5e0b1f32118b6af8b0
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-02-12 22:23:24 +0200

    targil

--- L08/myShell.c
+++ L08/myShell.c
@@ -22,7 +22,25 @@ int main()
     }
     return 0;
 }
-
+// יש לכתוב את פונקציית הברוכים הבאים כרצונכם אבל קצת יותר ממה שמוצג מטה לדוגמא:
+//                     aSPY//YASa       
+//              apyyyyCY//////////YCa       |
+//             sY//////YSpcs  scpCY//Pp     | Welcome to myShell
+//  ayp ayyyyyyySCP//Pp           syY//C    | Version 2.4.3
+//  AYAsAYYYYYYYY///Ps              cY//S   |
+//          pCCCCY//p          cSSps y//Y   | https://github.com/<user>
+//          SPPPP///a          pP///AC//Y   |
+//               A//A            cyP////C   | Have fun!
+//               p///Ac            sC///a   |
+//               P////YCpc           A//A   | Craft packets like it is your last
+//        scccccp///pSP///p          p//Y   | day on earth.
+//       sY/////////y  caa           S//P   |                      -- Lao-Tze
+//        cayCyayP//Ya              pY/Ya   |
+//         sY/PsY////YCc          aC//Yp 
+//          sc  sccaCY//PCypaapyCP//YSs  
+//                   spCPY//////YPSps    
+//                        ccaacs         
+//                                        using c 
 void welcome()
 {
     puts("Welcome to my Shell");
