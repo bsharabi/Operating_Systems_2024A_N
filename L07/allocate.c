ec710a9e49c0c3c60704adc7acb661d6836560e8
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-02-12 21:08:16 +0200

    myShell

--- L07/allocate.c
+++ L07/allocate.c
@@ -4,35 +4,33 @@
 #include <stdlib.h>
 int main()
 {
-        
-        int num = 10;
-       
 
-        char *s = (char *)malloc(num * sizeof(char));
-        int *i = (int *)malloc(num * sizeof(int));
+      int num = 10;
 
-        char *s1 = (char *)calloc(num, sizeof(char));
-        int *i1 = (int *)calloc(num, sizeof(int));
+      char *s = (char *)malloc(num * sizeof(char));
+      int *i = (int *)malloc(num * sizeof(int));
 
-        s1 = realloc(s1, 5);
-        char ch;
+      char *s1 = (char *)calloc(num, sizeof(char));
+      int *i1 = (int *)calloc(num, sizeof(int));
 
-        int size=1;
-        int index=0;
-        char * str =(char*)malloc(size*sizeof(char));
-        puts("enter char");
-        while ((ch = getchar()) != '\n')
-        {
-              *(str+index)=ch;  
-              size++;
-              index++;
-              str=(char*)realloc(str,size);
-        }
-        *(str+index)='\0';
-        // str[index]='\0';
-        puts(str);
-        printf("%ld\n",strlen(str));
-        
+      s1 = realloc(s1, 5);
+      char ch;
 
-        return 0;
+      int size = 1;
+      int index = 0;
+      char *str = (char *)malloc(size * sizeof(char));
+      puts("enter char");
+      while ((ch = getchar()) != '\n')
+      {
+            *(str + index) = ch;
+            size++;
+            index++;
+            str = (char *)realloc(str, size);
+      }
+      *(str + index) = '\0';
+      // str[index]='\0';
+      puts(str);
+      printf("%ld\n", strlen(str));
+
+      return 0;
 }
