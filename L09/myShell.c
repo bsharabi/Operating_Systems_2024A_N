dedbcc79f524e8a912e24fe1ccc657aefc4bd518
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-02-19 22:07:37 +0200

    shell

---
+++ L09/myShell.c
@@ -0,0 +1,31 @@
+#include "myShell.h"
+#include "myFunction.h"
+
+int main()
+{
+
+    welcome();
+    while (1)
+    {
+        getLocation();
+        char *input = getInputFromUser();
+        if (strcmp(input, "exit") == 0 || strncmp(input, "exit ", 5) == 0)
+            logout(input);
+
+        char **arguments = splitArgument(input);
+
+        if (strcmp(input, "echo") == 0)
+            echo(arguments);
+        else if (strcmp(input, "cd") == 0)
+            cd(arguments);
+        else if (strcmp(input, "cp") == 0)
+            cp(arguments);
+        free(arguments);
+        free(input);
+    }
+    return 0;
+}
+void welcome()
+{
+    puts("Welcome to my Shell");
+}
